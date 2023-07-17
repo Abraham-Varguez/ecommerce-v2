55ee1e647588a38df97cfff8c822cca24e9a5119
Abraham Varguez <abraham@Administrators-MacBook-Pro.local>
2023-07-17 08:13:43 -0400

    98%

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@
 
 //ES6 Module Type
 import express from "express";
+import path from 'path'
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 dotenv.config();
@@ -18,16 +19,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Cookir Parser Middleware
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use("/api/products", prodcutRoutes);
 app.use("/api/users", userRoutes);
 
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  //any route that is not api will be redirected to index.html
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
-
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
